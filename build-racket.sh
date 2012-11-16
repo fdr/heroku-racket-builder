d1af426080e160d5268a3256d19956640a17c2db
Daniel Farina <daniel@heroku.com>
2012-11-16 15:13:55 -0800

    Move binaries to the right spot
    Otherwise the tortuously long build process simply results in a nearly
    empty slug.
    
    Signed-off-by: Daniel Farina <daniel@heroku.com>

--- build-racket.sh
+++ build-racket.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -uex
 
+root=$(pwd)
+
 cd /tmp
 curl -L10 -O https://github.com/plt/racket/archive/master.tar.gz
 tar -zxf master.tar.gz
@@ -8,3 +10,5 @@ cd racket-master/src
 ./configure --prefix=/app/vendor
 make -sj30
 make -sj30 install
+
+mv /app/vendor $root/
