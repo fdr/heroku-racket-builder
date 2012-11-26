99c8d06beb341517ccd60acd897596436e171989
Daniel Farina <daniel@fdr.io>
2012-11-25 22:21:21 -0800

    explicit Make before make install
    Signed-off-by: Daniel Farina <daniel@fdr.io>

--- build-racket.sh
+++ build-racket.sh
@@ -8,6 +8,7 @@ curl -L10 -O https://github.com/plt/racket/archive/master.tar.gz
 tar -zxf master.tar.gz
 cd racket-master/src
 ./configure --prefix=/app/vendor --disable-docs
+make -sj30
 make -sj30 plain-install
 
 mv /app/vendor $root/
