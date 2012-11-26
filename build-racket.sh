9d1788d061b634955a10094ba854d1a90da779d4
Daniel Farina <daniel@fdr.io>
2012-11-25 22:18:43 -0800

    Try plain-install make target
    Signed-off-by: Daniel Farina <daniel@fdr.io>

--- build-racket.sh
+++ build-racket.sh
@@ -8,7 +8,6 @@ curl -L10 -O https://github.com/plt/racket/archive/master.tar.gz
 tar -zxf master.tar.gz
 cd racket-master/src
 ./configure --prefix=/app/vendor --disable-docs
-make -sj30
-make -sj30 install
+make -sj30 plain-install
 
 mv /app/vendor $root/
