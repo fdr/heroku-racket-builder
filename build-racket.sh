e050352b8e49f0ef06e4cc7b4905cfe24404be46
Daniel Farina <daniel@fdr.io>
2012-11-25 23:19:13 -0800

    Remove questionable collects
    Signed-off-by: Daniel Farina <daniel@fdr.io>

--- build-racket.sh
+++ build-racket.sh
@@ -6,6 +6,51 @@ root=$(pwd)
 cd /tmp
 curl -L10 -O https://github.com/plt/racket/archive/master.tar.gz
 tar -zxf master.tar.gz
+
+# Clean collets of questionable use in industry.  These blow up the
+# emitted slug and can cause timeouts.
+for collect in "2htdp
+algol60
+at-exp
+browser
+deinprogramm
+drracket
+drscheme
+embedded-gui
+eopl
+framework
+future-visualizer
+games
+graphics
+gui-debugger
+handin-client
+handin-server
+help
+hierlist
+honu
+htdp
+icons
+images
+macro-debugger
+mred
+mrlib
+mysterx
+mzcom
+mzlib
+mzscheme
+picturing-programs
+plai
+r5rs
+r6rs
+rnrs
+scheme
+schemeunit
+sgl
+swindle
+teachpack
+wxme"
+do rm -fr racket-master/collects/${collect}; done;
+
 cd racket-master/src
 ./configure --prefix=/app/vendor --disable-docs --disable-gracket
 make -sj30
