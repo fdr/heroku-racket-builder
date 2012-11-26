31d12f2354143e66b5d5ad0372e3bcd23a82b091
Daniel Farina <daniel@fdr.io>
2012-11-25 23:19:22 -0800

    Disable gracket
    Signed-off-by: Daniel Farina <daniel@fdr.io>

--- build-racket.sh
+++ build-racket.sh
@@ -7,7 +7,7 @@ cd /tmp
 curl -L10 -O https://github.com/plt/racket/archive/master.tar.gz
 tar -zxf master.tar.gz
 cd racket-master/src
-./configure --prefix=/app/vendor --disable-docs
+./configure --prefix=/app/vendor --disable-docs --disable-gracket
 make -sj30
 make -sj30 plain-install
 
