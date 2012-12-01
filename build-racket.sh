e6dce46b71b03200fc3b6063a6820b5f692f8801
Daniel Farina <daniel@fdr.io>
2012-12-01 14:14:21 -0800

    Try explicit raco setup
    Signed-off-by: Daniel Farina <daniel@fdr.io>

--- build-racket.sh
+++ build-racket.sh
@@ -7,8 +7,9 @@ cd /tmp
 curl -L10 -O https://github.com/plt/racket/archive/master.tar.gz
 tar -zxf master.tar.gz
 cd racket-master/src
-./configure --prefix=/app/vendor --disable-gracket
-make -sj30
-make -sj30 install
+./configure --prefix=/app/vendor --disable-gracket --disable-docs
+make -sj5
+make -sj5 install
+/app/vendor/bin/raco setup
 
 cp -a /app/vendor $root/
